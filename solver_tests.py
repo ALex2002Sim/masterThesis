c472d80febe3b0f5d67f6242ed63c07a4943862f
Alexander Simakov <macbook@MacBook-Air-macbook.local>
2025-02-25 16:59:02 +0300

    new_graphs

--- solver_tests.py
+++ solver_tests.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
         
         sol = IntEq(test, func)
         time = sol.solutionGraph()
-        sol.errorGraph()
+        #sol.errorGraph()
         print(f"Estimated time: {time}")
 
